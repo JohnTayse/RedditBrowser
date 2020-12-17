6713f655ed94c5d9e82860dcd1b21c5c5bbf3d63
John Tayse <johntayse@Johns-MacBook-Pro.local>
2020-12-17 17:38:20 -0500

    Changes to Favorites page

--- RedditBrowser/modules/components/favorites.js
+++ RedditBrowser/modules/components/favorites.js
@@ -7,6 +7,30 @@ function importFavorites(){
 	FavoritesComponent.render();
 }
 
+function exportFavorites(){
+	var favorites = browser.getFavorites();
+	var favoritesText = '';
+	var subredditFavs = favorites.filter(x => x.substring(0, 2) !== 'u/');
+	var userFavs = favorites.filter(x => x.substring(0, 2) === 'u/');
+	subredditFavs.forEach(subreddit =>
+		favoritesText += subreddit + '\n'
+	)
+	userFavs.forEach(user =>
+		favoritesText += user + '\n'
+	)
+
+	$('#favoritesInput').text(favoritesText);
+
+	var clearButton = '<button id="clearButton" class="ui-btn ui-btn-corner-all ui-btn-inline">Clear</button>';
+
+	$('#favoritesInput').after(clearButton);
+
+	$('#importButton').hide();
+	$('#exportButton').hide();
+
+	$('#clearButton').click(function(){ FavoritesComponent.render() });
+}
+
 function removeFavorite(row){
 	var favorite = row.getAttribute('fav');
 	browser.removeFavorite(favorite);
@@ -31,11 +55,13 @@ const FavoritesComponent = {
 			<div id="favorites">
 				<textarea placeholder="favorites for import" id="favoritesInput" style="color:black;"></textarea>
 				<button id="importButton" class="ui-btn ui-btn-corner-all ui-btn-inline">Import</button>
+				<button id="exportButton" class="ui-btn ui-btn-corner-all ui-btn-inline">Export</button>
 				` + favoritesHtml + `
 			</div>
 		`;
 		$('#app').html(section).trigger('create');
 		$('#importButton').click(function(){ importFavorites() });
+		$('#exportButton').click(function(){ exportFavorites() });
 		$('.remove').click(function(){ removeFavorite(this) });
 	}
 } 
